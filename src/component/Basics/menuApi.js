1e73ab9b765b3c4a53e099f174374e74942210f9
subhadipshee <subhadipshee20010521@gmail.com>
2023-02-10 20:30:01 +0530

    updated

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://cdn.shopify.com/s/files/1/2675/9476/files/custom_resized_9cc03a48-813b-482a-952d-f163a1cb4314_2048x2048.jpg?v=1623401617",
+      name: "Tea",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "I love Tea realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "https://media.istockphoto.com/id/177314242/photo/vegetable-pakora.jpg?s=612x612&w=0&k=20&c=iiVKreSzqvNN7UxGaDUw3HKU5jK-htT1DppUA89QtyE=",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "https://st2.depositphotos.com/1008239/9737/i/450/depositphotos_97375690-stock-photo-two-toast-bread-with-eggs.jpg",
+      name: "dim tost",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "https://st2.depositphotos.com/3147737/5188/i/600/depositphotos_51888321-stock-photo-hyderabadi-biryani-a-popular-chicken.jpg?_gl=1*18q77ov*_ga*MjE0MTc3OTkwLjE2NzYwMTY5ODc.*_ga_VD9K87Z6E1*MTY3NjAxNjk4OS4xLjEuMTY3NjAxNzMyOS4wLjAuMA..",
+      name: "Biriyani",
+      category: "lunch",
+      price: "150₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=60",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "https://media.istockphoto.com/id/1363066353/photo/bhatti-ka-murgh-is-an-authentic-punjabi-recipe-that-is-extremely-loved-by-chicken-lovers-all.jpg?s=1024x1024&w=is&k=20&c=R55WTDWZ8aBLBs3bQ942RdJaXDcEe9j9iQCOL7dsrf8=",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT07Wb2D-uiAF-UMn5z3_8iomB5E7n5GV_Qzg&usqp=CAU",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "https://thumbs.dreamstime.com/b/rajma-chawal-rice-21781096.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "https://static.toiimg.com/thumb/55453664.cms?imgsize=317782&width=800&height=800",
+      name: "samasa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
