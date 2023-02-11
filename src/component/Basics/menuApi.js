44566cb0ed7c59a14a4d57b0a1077c47239c17c7
subhadipshee <subhadipshee20010521@gmail.com>
2023-02-11 22:13:33 +0530

    updated

--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -6,13 +6,13 @@ const Menu = [
       category: "breakfast",
       price: "10₹",
       description:
-        "I love Tea realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
-    },
-  
+        "Where there’s tea, there’s happinessTeatime: A hug for the soul and spiritA cup of tea a day keeps worries away.Tea is a hug in a cup.,Tea-riffic!Leave me be, I’m drinking my tea.,If tea can’t fix it, it's a serious problem.,Hit me with your best pot I think there’s a tea for that  "
+    }
+  ,
     {
       id: 2,
       image: "https://media.istockphoto.com/id/177314242/photo/vegetable-pakora.jpg?s=612x612&w=0&k=20&c=iiVKreSzqvNN7UxGaDUw3HKU5jK-htT1DppUA89QtyE=",
-      name: "allu pakoida",
+      name: "Allu pakoida",
       category: "evening",
       price: "20₹",
       description:
@@ -23,9 +23,11 @@ const Menu = [
       image: "https://st2.depositphotos.com/1008239/9737/i/450/depositphotos_97375690-stock-photo-two-toast-bread-with-eggs.jpg",
       name: "dim tost",
       category: "breakfast",
-      price: "10₹",
+      price: "30₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        
+          "A single egg toast makes a super satisfying and healthy breakfast, because it's loaded with veggies, a little fat from the feta and protein from the eggs, but if you need a little something extra, add a drink."
+        ,
     },
     {
       id: 4,
@@ -61,7 +63,7 @@ const Menu = [
       category: "dinner",
       price: "60₹",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+        "Gulab jamun (also spelled gulaab jamun; lit. 'Rose water berry' or 'Rose berry') is a sweet confectionery or dessert, originating in the Indian subcontinent and a type of mithai popular in India, Nepal, the Maldives (where it is known as gulab ki janu), and Bangladesh, as well as Myanmar. . ",
     },
     {
       id: 8,
@@ -75,12 +77,50 @@ const Menu = [
     {
       id: 9,
       image: "https://static.toiimg.com/thumb/55453664.cms?imgsize=317782&width=800&height=800",
-      name: "samasa",
+      name: "samosa",
       category: "evening",
       price: "10₹",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    }
+    ,
+    {
+      id: 10,
+      image: "https://media.gettyimages.com/id/157528129/photo/mug-on-plate-filled-with-coffee-surrounded-by-coffee-beans.jpg?s=612x612&w=gi&k=20&c=z6G82Aghr4kRQRVTpyD5izhsckR0DiyviBBwD38UlZ0=",
+      name: "coffe",
+      category: "breakfast",
+      price: "25₹",
+      description:
+        "Coffee is a beloved beverage known for its ability to fine-tune your focus and boost your energy levels.In fact, many people depend on their daily cup of joe right when they wake up to get their day started on the right foot. In addition to its energizing effects, coffee has been linked to a long list of potential health benefits, giving you all the more reason to get brewing.This article takes an in-depth look at 9 of the top evidence-based benefits of coffee",
+    }
+    ,
+    {
+      id: 11,
+      image: "https://media.istockphoto.com/id/597657478/photo/like-tea.jpg?s=612x612&w=0&k=20&c=PgfvY_uI6B1K3FYV_wNen0hC32JVk6Mhm0yKIrFn6tI=",
+      name: "Green Tea",
+      category: "breakfast",
+      price: "25₹",
+      description:
+        "Green tea is loaded with antioxidants. Among its benefits, it may help support brain function and fat loss, help reduce the chance of developing heart disease, and offer some protection against cancer",
     },
+    {
+      id: 12,
+      image: "https://media.istockphoto.com/id/629831068/photo/enjoy-your-chinese-dumplings-in-wooden-steamer.jpg?s=612x612&w=0&k=20&c=8ektrtoyO4AbJrojQW6-qmg_n8wTwg3sWLOJpXm3pSg=",
+      name: "Momo",
+      category: "evening",
+      price: "250₹",
+      description:
+        "Momo is a beloved beverage known for its ability to fine-tune your focus and boost your energy levels.In fact, many people depend on their daily cup of joe right when they wake up to get their day started on the right foot. In addition to its energizing effects, coffee has been linked to a long list of potential health benefits, giving you all the more reason to get brewing.This article takes an in-depth look at 9 of the top evidence-based benefits of coffee",
+    },
+    {
+      id: 13,
+      image: "https://media.istockphoto.com/id/1009279168/photo/pasta-with-meat-and-vegetables.jpg?s=612x612&w=0&k=20&c=nV8FrAKQUWBhlFdnw-yQmuxRYACPRh6EXi0YHWvddC0=",
+      name: "chines",
+      category: "evening",
+      price: "250₹",
+      description:
+        "chines is a beloved beverage known for its ability to fine-tune your focus and boost your energy levels.In fact, many people depend on their daily cup of joe right when they wake up to get their day started on the right foot. In addition to its energizing effects, coffee has been linked to a long list of potential health benefits, giving you all the more reason to get brewing.This article takes an in-depth look at 9 of the top evidence-based benefits of coffee",
+    }
   ];
   
   export default Menu;
